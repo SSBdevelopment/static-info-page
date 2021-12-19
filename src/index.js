f307261f02123c9a5d40cd8cab921aa9eed226a8
SketchyJelly <stephenbrown.business@gmail.com>
2021-12-19 12:49:11 +1100

    Render const navBar to root
    Learning the <nav> element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
+const navBar = (
+    <nav>
+        <h1>Heading</h1>
+        <ul>
+            <li>Page 1</li>
+            <li>Page 2</li>
+            <li>Page 3</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navBar, document.getElementById("root"))
