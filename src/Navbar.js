c5b3261d28fd5e16d4ec51b3741d9fcf8f57bd08
SketchyJelly <stephenbrown.business@gmail.com>
2021-12-20 12:22:49 +1100

    Project href added

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav>
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1 by <a href="">Stephen</a></h4>
+            <h4 className="nav--title">React Course - Project 1 Completed by <a href="https://github.com/SSBdevelopment/static-info-page">Stephen</a></h4>
         </nav>
     )
 }
